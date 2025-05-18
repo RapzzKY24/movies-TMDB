e60b05416d1f341cbf4c6b457cdcda7188d1a26e
RapzzKY24 <paymeriskiaulia@gmail.com>
2025-05-18 19:38:38 +0700

    update

--- movies-tmdb/src/App.jsx
+++ movies-tmdb/src/App.jsx
@@ -6,6 +6,8 @@ import SearchResultsPage from "./Pages/SearchResultsPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 import LoginPages from "./Pages/LoginPages";
 import SignUpPage from "./Pages/SignUpPages";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import PublicRoute from "./Components/PublicRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
@@ -14,11 +16,51 @@ function App() {
     <div className="app-container">
       <Container fluid className="p-0">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movie/:id" element={<MovieDetailPage />} />
-          <Route path="/search" element={<SearchResultsPage />} />
-          <Route path="/*" element={<LoginPages />} />
-          <Route path="/signup" element={<SignUpPage />} />
+          {/* Protected Routes - Only accessible when logged in */}
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/movie/:id" 
+            element={
+              <ProtectedRoute>
+                <MovieDetailPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/search" 
+            element={
+              <ProtectedRoute>
+                <SearchResultsPage />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Public Routes - Only accessible when logged out */}
+          <Route 
+            path="/login" 
+            element={
+              <PublicRoute>
+                <LoginPages />
+              </PublicRoute>
+            } 
+          />
+          <Route 
+            path="/signup" 
+            element={
+              <PublicRoute>
+                <SignUpPage />
+              </PublicRoute>
+            } 
+          />
+
+          {/* Always accessible */}
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Container>
@@ -27,4 +69,5 @@ function App() {
 }
 
 export default App;
+
 
---
+++ movies-tmdb/src/Components/PublicRoute.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "../Config/firebase";
+
+const PublicRoute = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setAuthenticated(!!user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    // You can show a loading spinner here
+    return <div className="loading-container">Loading...</div>;
+  }
+
+  if (authenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default PublicRoute;

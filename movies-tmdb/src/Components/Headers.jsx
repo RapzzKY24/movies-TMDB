829347af216dc6d491aeb3a68e0c80f589f47378
RapzzKY24 <paymeriskiaulia@gmail.com>
2025-05-18 15:17:14 +0700

    update

--- movies-tmdb/src/Components/Headers.jsx
+++ movies-tmdb/src/Components/Headers.jsx
@@ -24,7 +24,7 @@ const Headers = ({ handleSearch }) => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/" className="nav-link"></Nav.Link>
                     </Nav>
                     {handleSearch && <SearchBar onSearch={handleSearch} />}
                 </Navbar.Collapse>
